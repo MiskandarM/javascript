0660d4554f391a67c61b12ac4d4b2bb12f051d94
ISKANDAR <iskandar@sepulsa.com>
2018-05-12 17:04:35 +0700

    belajar if else

---
+++ if-else.js
@@ -0,0 +1,18 @@
+/*
+Belajar if - else di javascript
+12 Mei 2018
+*/
+
+function kondisiSatu(){
+  var nilai = 3;
+
+  if (nilai === 3){
+      console.log("Benar")
+    } else if (nilai === 2) {
+      console.log("Dua")
+  } else {
+  console.log("Salah")
+  }
+}
+
+kondisiSatu()
