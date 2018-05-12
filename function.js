3532716bd927f6cfcd53e5668b64770e6e8eac5e
ISKANDAR <iskandar@sepulsa.com>
2018-05-12 15:23:06 +0700

    Penulisan tiga jenis function

---
+++ function.js
@@ -0,0 +1,20 @@
+/*
+Belajar membuat function di javascript
+*/
+
+function belajarSatu(){
+  console.log("Function jenis satu");
+}
+
+const belajarDua = function(){
+  console.log("Function jenis dua");
+}
+
+belajarSatu()
+belajarDua()
+
+const belajarTiga = ()=> {
+  console.log("Function jenis 3, penulisan baru");
+}
+
+belajarTiga()
