c94e9adec03fbb855bfc90df98866498af39cfbf
ISKANDAR <iskandar@sepulsa.com>
2018-05-17 09:48:14 +0700

    file switch

---
+++ switch.js
@@ -0,0 +1,20 @@
+/*
+Belajar switch
+*/
+
+function belajarSatu(){
+  var nilai = 5;
+
+  switch (nilai) {
+    case 10:
+      console.log("Sangat memuaskan");
+      break;
+    case 8:
+      console.log("Bagus");
+      break;
+    default:
+      console.log("Lainnya")
+  }
+}
+
+belajarSatu()
