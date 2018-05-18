0ff941ea58e815ea8e05721ed97287d1568e1747
ISKANDAR <iskandar@sepulsa.com>
2018-05-18 21:17:03 +0700

    belajar array

---
+++ array.js
@@ -0,0 +1,11 @@
+function array(){
+  var angka = [1,2,3,4,6];
+  var nama = ["darma", "iskandar", "maulana"];
+
+  console.log(angka.length)
+  console.log(nama.length)
+
+  console.log(nama[2])
+}
+
+array()
